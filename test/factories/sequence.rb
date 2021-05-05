4199acf2e3b3b48eaa6b725572fbb7c0db515512
Elza.Sharafutdinova <elza.shar@gmail.com>
2021-05-05 11:55:34 +0000

    Added assosiations and validations

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,9 +1,13 @@
 FactoryBot.define do
-  sequence :string, aliases: [:first_name, :last_name, :password, :email, :avatar, :type, :name, :description, :state] do |n|
+  sequence :string, aliases: [:first_name, :last_name, :password, :avatar, :type, :name, :description, :state] do |n|
     "string#{n}"
   end
 
-  sequence :date, aliases: [:expired_at] do |n| 
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+
+  sequence :date, aliases: [:expired_at] do |_n|
     Time.now
   end
 end
